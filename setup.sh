80e581933a2cd5964a8572ecf3d19b877fbf5f05
Eric <ecuyle@gmail.com>
2020-02-06 22:53:28 -0800

    Update setup script to use separate repo for .spacemacs config

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/bash
 
+# Remove old dirs
+rm -rf source-code-pro
+rm -rf dotfile-spacemacs
+
 # Install latest emacs
 sudo pacman -Syy emacs
 
+# Save current directory
+CURR_DIR=$PWD
+
 # Install Source Code Pro font
-cd ~
+cd $CURR_DIR
 git clone https://github.com/adobe-fonts/source-code-pro.git
 cd source-code-pro
 sudo cp ./TTF/* /usr/share/fonts
@@ -18,12 +25,16 @@ mv .emacs .emacs.bak
 # Clone spacemacs
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
-# Run emacs for setup
-emacs --insecure
-
 # Copy over my personal .spacemacs file for configs
+cd $CURR_DIR
+git clone git@github.com:ecuyle/dotifle-spacemacs.git
+cd dotfile-spacemacs
 cp ~/.spacemacs ~/.spacemacs.bak
 cp ./.spacemacs ~/.spacemacs
 
 # Re-run emacs to install custom .spacemacs configs
 emacs --insecure
+
+# Remove old dirs
+rm -rf source-code-pro
+rm -rf dotfile-spacemacs
